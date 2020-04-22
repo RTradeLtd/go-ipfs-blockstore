db7cace887943417a6106a21c4c7891dfec57011
postables <postables@rtradetechnologies.com>
2020-04-22 01:00:43 -0700

    reference counted blockstore interface

--- blockstore.go
+++ blockstore.go
@@ -1,5 +1,3 @@
-// Package blockstore implements a thin wrapper over a datastore, giving a
-// clean interface for Getting and Putting block objects.
 package blockstore
 
 import (
---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package blockstore implements a thin wrapper over a datastore, giving a
+// clean interface for Getting and Putting block objects.
+package blockstore
